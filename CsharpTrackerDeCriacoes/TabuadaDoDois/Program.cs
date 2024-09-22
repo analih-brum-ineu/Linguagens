0e37569a00ac38619da3b055d5bc5ccfa40b855d
Analih Brum Ineu <analihineu@hotmail.com>
2024-09-22 17:49:54 -0300

    Create Program.cs

---
+++ CsharpTrackerDeCriacoes/TabuadaDoDois/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace TabuadaDoDois
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + " x 2 = " + i * 2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
