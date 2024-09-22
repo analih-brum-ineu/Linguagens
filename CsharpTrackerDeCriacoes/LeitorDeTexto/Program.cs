ac870ccf4bd42e7584e08ff30458ef2fb09e5589
Analih Brum Ineu <analihineu@hotmail.com>
2024-09-22 17:34:54 -0300

    Create Program.cs

---
+++ CsharpTrackerDeCriacoes/LeitorDeTexto/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeitorDeTexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
