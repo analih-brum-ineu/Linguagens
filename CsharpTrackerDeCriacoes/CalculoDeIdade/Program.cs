d9fd5620df1b6d7b5b234aa0e2df23710173f491
Analih Brum Ineu <analihineu@hotmail.com>
2024-09-22 22:52:56 -0300

    Update Program.cs

--- CsharpTrackerDeCriacoes/CalculoDeIdade/Program.cs
+++ CsharpTrackerDeCriacoes/CalculoDeIdade/Program.cs
@@ -7,14 +7,14 @@ namespace CalculoDeIdade
         static void Main(string[] args)
         {
             Console.Write("Informe o ano de nascimento: ");
-            int Ano = Int32.Parse(Console.ReadLine());
+            int ano = Int32.Parse(Console.ReadLine());
 
-            int AnoAtual = DateTime.Now.Year;
+            int anoAtual = DateTime.Now.Year;
 
-            if (Ano < AnoAtual)
+            if (ano < anoAtual)
             {
-                int Idade = AnoAtual - Ano;
-                Console.WriteLine("Idade = " + Idade + " anos");
+                int idade = anoAtual - ano;
+                Console.WriteLine("Idade = " + idade + " anos");
             }
             else { Console.WriteLine("O ano informado é inválido!"); }
             Console.ReadLine();
