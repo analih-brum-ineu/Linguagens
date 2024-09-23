4cd267a0837d5f213d1032252874c9e3e1cf756a
Analih Brum Ineu <analihineu@hotmail.com>
2024-09-22 22:19:05 -0300

    Create Program.cs

---
+++ CsharpTrackerDeCriacoes/ExibirMensagem/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ExibirData
+{
+    internal class Program
+    {
+        public static void ExibirMensagem(string Nome)
+        {
+            Console.WriteLine("Bem-vindo(a), {0}!", Nome);
+        }
+        static void Main(string[] args)
+        {
+            string Nome = "Analih Brum Ineu";
+            ExibirMensagem(Nome);
+
+            Console.ReadKey();
+        }
+    }
+}
