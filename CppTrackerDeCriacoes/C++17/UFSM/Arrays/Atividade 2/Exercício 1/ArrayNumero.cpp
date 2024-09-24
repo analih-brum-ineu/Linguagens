84273d13456621c0e08cf0cc9cfc2c56f41b2d92
Analih Brum Ineu <analihineu@hotmail.com>
2024-09-24 11:55:04 -0300

    Update ArrayNumero.cpp

--- CppTrackerDeCriacoes/C++17/UFSM/Arrays/Atividade 2/Exercício 1/ArrayNumero.cpp
+++ CppTrackerDeCriacoes/C++17/UFSM/Arrays/Atividade 2/Exercício 1/ArrayNumero.cpp
@@ -4,34 +4,34 @@
 
 int main()
 {
-	int a[TAMANHO];
+	int numero[TAMANHO];
 	int pesquisa, contador = 0;
 
 	for (int i = 0; i < TAMANHO; i++)
 	{
-		std::cout << "Informe o " << i + 1 << ".o numero: "; std::cin >> a[i];
+		std::cout << "Informe o " << i + 1 << ".o numero: "; std::cin >> numero[i];
 	}
 
 	for (int i = 0; i < TAMANHO - 1; i++)
 	{
 		for (int j = i + 1; j < TAMANHO; j++)
 		{
-			if (a[i] > a[j])
+			if (numero[i] > numero[j])
 			{
-				std::swap(a[i], a[j]);
+				std::swap(numero[i], numero[j]);
 			}
 		}
 	}
 
 	for (int i = 0; i < TAMANHO; i++)
 	{
-		std::cout << a[i] << std::endl;
+		std::cout << numero[i] << std::endl;
 	}
 
 	std::cout << "Informe o numero que procura: "; std::cin >> pesquisa;
 	for (int i = 0; i < TAMANHO; i++)
 	{
-		if (a[i] == pesquisa) { std::cout << "Numero encontrado: " << a[i] << std::endl; contador++; }
+		if (numero[i] == pesquisa) { std::cout << "Numero encontrado: " << numero[i] << std::endl; contador++; }
 	}
 	if (contador == 0) { std::cout << "Numero nao encontrado!" << std::endl; }
 
